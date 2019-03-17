be9a31bd4bbd909e4a0b7999b36089e973f8490f
TomTheToad <tomthetoad951@gmail.com>
2019-03-17 12:58:20 -0400

    Updated level1 to allow for testing more blocked spaces.

--- js/app.levels.js
+++ js/app.levels.js
@@ -7,9 +7,9 @@ let water = 'images/water-block.png';
 // 5 x 6 grid
 const level1 = [
     grass, grass, grass, grass, grass,
-    stone, stone, stone, stone, stone,
+    stone, stone, stone, water, stone,
     stone, stone, stone, stone, stone,
     water, stone, stone, stone, stone,
-    stone, stone, stone, stone, stone,
+    stone, stone, stone, stone, water,
     grass, grass, grass, grass, grass,
 ]
